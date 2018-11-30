6100205a0a1ef56070eb6154657685320c078800
Thomas Sundberg <tsu@kth.se>
2018-11-30 19:17:05 +0100

    Use the system to save a note

--- src/main/java/se/thinkcode/notes/Memory.java
+++ src/main/java/se/thinkcode/notes/Memory.java
@@ -1,4 +1,6 @@
 package se.thinkcode.notes;
 
 public class Memory {
+    public void save(Note note) {
+    }
 }
--- src/test/java/se/thinkcode/notes/NotesSteps.java
+++ src/test/java/se/thinkcode/notes/NotesSteps.java
@@ -5,16 +5,18 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class NotesSteps {
+    private Memory memory;
+    private Note note;
+
     @Given("she must remember to book Nalen")
     public void she_must_remember_to_book_Nalen() {
-        Memory memory = new Memory();
-        Note note = new Note("Remember to book Nalen");
+        memory = new Memory();
+        note = new Note("Remember to book Nalen");
     }
 
     @When("she writes the note")
     public void she_writes_the_note() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        memory.save(note);
     }
 
     @Then("should she be able to see the note")
