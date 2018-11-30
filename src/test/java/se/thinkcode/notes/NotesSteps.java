dc2695ed38232084524933f1baf45a5ab3b5c313
Thomas Sundberg <tsu@kth.se>
2018-11-30 19:06:20 +0100

    Added steps suggested by Cucumber

---
+++ src/test/java/se/thinkcode/notes/NotesSteps.java
@@ -0,0 +1,25 @@
+package se.thinkcode.notes;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class NotesSteps {
+    @Given("she must remember to book Nalen")
+    public void she_must_remember_to_book_Nalen() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("she writes the note")
+    public void she_writes_the_note() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("should she be able to see the note")
+    public void should_she_be_able_to_see_the_note() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
